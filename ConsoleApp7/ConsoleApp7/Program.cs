f9514c160ed50fd9b8d65262516fa75dc333b25c
BearlyCorporeal <48078310+BearlyCorporeal@users.noreply.github.com>
2019-04-11 15:28:13 +1000

    finished the arrays and lists submittable tasks
    consoleApp4 = part 1 of using arrays
    consoleApp5 = part 2 of using arrays
    consoleApp7 = part 1 of using lists
    consoleApp8 = part 2 of using lists

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int count2 = 0;
+            int input = 0;
+            List<int> numbers = new List<int>(5);
+            while( count < 5)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+                count = count + 1;
+            }
+            input = int.Parse(Console.ReadLine());
+            while (count2 < 5)
+            {
+                
+                if (input == numbers[count2])
+                {
+                    Console.WriteLine("that number has already been used");
+                    count2 = 6;
+                }
+                count2 = count2 + 1;
+            }
+            Console.ReadKey();
+           
+        
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int count2 = 0;
+            int input = 0;
+            int repeats = 0;
+            List<int> numbers = new List<int>(5);
+            while (count < 5)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+                count = count + 1;
+            }
+            input = int.Parse(Console.ReadLine());
+            while (count2 < 5)
+            {
+
+                if (input == numbers[count2])
+                {
+                    repeats = repeats + 1;
+                }
+                count2 = count2 + 1;
+            }
+            Console.WriteLine("you repeated this number " + repeats + " times");
+            Console.ReadKey();
+
+        }
+    }
+}
